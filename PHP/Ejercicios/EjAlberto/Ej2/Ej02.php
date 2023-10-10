cccd366413371ce87225a7217d6303f865b35df9
Daniel Guerrero <daniprogguerrero@gmail.com>
2023-10-11 00:52:24 +0200

    \$

---
+++ PHP/Ejercicios/EjAlberto/Ej2/Ej02.php
@@ -0,0 +1,14 @@
+<?php
+    print("<h1>EJERCICIO 2</h1>"); /*2. Write a PHP script to display the following strings.
+                                        Sample String :
+                                        'Tomorrow I \'ll learn PHP global variables.'
+                                        'This is a bad command : del c:\\*.*'
+                                        Expected Output :
+                                        Tomorrow I 'll learn PHP global variables.
+                                        This is a bad command : del c:\*.*
+*/
+    $x1 = 'Tomorrow I \'ll learn PHP global variables.';
+    $x2 = 'This is a bad command : del c:\\*.*';
+
+    echo "$x1 <br> $x2";
+?>
