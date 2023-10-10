d906da88ec28ed94296957b376ab441ad83614c8
Monkey D. Luffy <dguerei620@g.educaand.es>
2023-10-10 08:59:19 +0200

    Addition

--- PHP/Ejercicios/EjAlberto/Ej1/Ej1.php
+++ PHP/Ejercicios/EjAlberto/Ej1/Ej1.php
@@ -1,5 +1,5 @@
 <?php
-   print("<h1>EJERCICIO 1</h1>"); //Crea 2 variables llamadas $x1 y $x2. Almacena un valor numérico en ellas y posteriormente muestra en pantalla la suma de ellas.
+   print("<h1>EJERCICIO 1</h1>"); //1. Crea 2 variables llamadas $x1 y $x2. Almacena un valor numérico en ellas y posteriormente muestra en pantalla la suma de ellas.
    
     $x1 = 5;
     $x2 = 10;
