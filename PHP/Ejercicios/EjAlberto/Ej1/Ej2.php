e835ef154c50a7e63adbd8d890d7552bf80fb41a
Monkey D. Luffy <dguerei620@g.educaand.es>
2023-10-10 08:58:38 +0200

    Array $frutas

--- PHP/Ejercicios/EjAlberto/Ej1/Ej2.php
+++ PHP/Ejercicios/EjAlberto/Ej1/Ej2.php
@@ -1,19 +1,17 @@
 <?php
-    print("<h1>Ejercicio 2</h1>"); 
-   /*Completa el siguiente código para que muestre el número de elementos del array $frutas:  
-    <?php 
-        $frutas = array("Manzana", "Plátano", "Naranja"); 
-        //Mostrar el número de elementos del array anterior 
-    ?> 
+    print("<h1>Ejercicio 2</h1>"); /*2. Completa el siguiente código para que muestre el número de elementos del array $frutas:  
+                                        <?php 
+                                            $frutas = array("Manzana", "Plátano", "Naranja"); 
+                                            //Mostrar el número de elementos del array anterior 
+                                        ?> 
 */
     $frutas = array("Manzana", "Plátano", "Naranja"); 
 
     for ( $i = 0; $i < count($frutas); $i++ ) {
-        print $frutas[$i];
-        echo "<br>";
-    }
-
-    echo "<br>";
+        print $frutas[$i] . "<br>";
+    } echo "<br>";
+    
+    
     echo "<h2>Otra solución</h2>";
     var_dump($frutas);
 ?>
