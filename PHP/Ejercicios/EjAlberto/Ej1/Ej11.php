14d65a4d7d3e1320ef9e1b5f2863ef6ad9db5bd8
Monkey D. Luffy <dguerei620@g.educaand.es>
2023-10-10 09:09:11 +0200

    STOCK

---
+++ PHP/Ejercicios/EjAlberto/Ej1/Ej11.php
@@ -0,0 +1,6 @@
+<?php
+    print("<h1>EJERCICIO 11</h1>"); //11. Crea una constante llamada STOCK que tenga el valor 100  y que distinga entre minúsculas/mayúsculas 
+
+    define("STOCK", 100);
+    echo "El valor de la constante STOCK es: " . STOCK;
+?>
