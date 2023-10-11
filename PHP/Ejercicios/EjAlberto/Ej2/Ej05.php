3e32a3418ea3fa1cdc0bf1a9df229c6d3d1194be
Monkey D. Luffy <dguerei620@g.educaand.es>
2023-10-11 13:11:11 +0200

    Sum digits

--- PHP/Ejercicios/EjAlberto/Ej2/Ej05.php
+++ PHP/Ejercicios/EjAlberto/Ej2/Ej05.php
@@ -1,17 +1,37 @@
 <?php
-    print("<h1>EJERCICIO 4</h1>"); //5. Write a PHP program to compute the sum of the digits of a number.
+    print("<h1>EJERCICIO 5</h1>"); //5. Write a PHP program to compute the sum of the digits of a number.
 
-    function sumDigits($dig) {
+    function sumDigits1($dig) {
         if(is_numeric($dig)){ $dig = strval($dig); } //Si pasan los dígitos en forma numeric este los transformará en string
 
         $sumdig = 0;
-        for ($i = 0; $i < 5; $i++) {
+        for ($i = 0; $i < strlen($dig); $i++) {
             $sumdig += $dig[$i];    
         } 
         echo "La suma de los dígitos es: $sumdig";
     }
 
-    sumDigits("12345");
-    sumDigits(11111);
+    sumDigits1("12345");    echo "<br>";
+    sumDigits1(11111);
 
+
+
+
+
+    print("<h2>Otra solución</h2>");
+
+    function sumDigits2($dig) {
+        $sumdig = 0;
+        //DEFINE("digLength", strlen($dig));
+        $l = strlen($dig);
+        for ($i = 0; $i < $l; $i++) {
+            $lastdig = $dig % 10;
+            $sumdig += $lastdig;
+            $dig = (int) ($dig/10);
+        }
+        echo "La suma de los dígitos del número es: " . $sumdig;
+    }
+
+    sumDigits2("123455");    echo "<br>";
+    sumDigits2(12345);
 ?>
