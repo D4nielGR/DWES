76ce9e6abfc12e44b26207f7ac91a61016524db5
Daniel Guerrero <daniprogguerrero@gmail.com>
2023-10-18 20:22:31 +0200

    Show list

--- PHP/Ejercicios/ProAlberto/P01/index.php
+++ PHP/Ejercicios/ProAlberto/P01/index.php
@@ -5,7 +5,8 @@
     
     $item1 = new Item("Manzana", 30, 1);
     $item2 = new Item("Plátano", 20, 2);
-    array_push($arrayItems, $item1, $item2);
+    $item3 = new Item("Peras", 10, 3);
+    array_push($arrayItems, $item1, $item2, $item3);
 
     class Item{
         public $name;
@@ -40,45 +41,46 @@
     </header>
 
     <body>
-        <table border="2px" align="center">
-            <tr>
-                <th>Name</th>
-                <th>Quantity</th>
-                <th>Price</th>
-                <th>Total</th>
-            </tr>
+        <?php
+            if (empty($arrayItems)) { echo "<p>No existe ningún Item actuamente</p>"; }
+            else {
+                echo "<table border='2px' align='center'>
+                        <tr>
+                            <th>Name</th>
+                            <th>Quantity</th>
+                            <th>Price</th>
+                            <th>Total</th>
+                        </tr>";
 
-            <?php
-                //<tr> 
                 $totalItem = 0;
                 for ($i = 0; $i < count($arrayItems); $i++) {
                     $total = $arrayItems[$i]->getQuantity() * $arrayItems[$i]->getPrice();
                     $totalItem += $total;
 
-                    echo("<tr align='center'>");
-                        echo ("<td> <p>{$arrayItems[$i]->getName()}</p> </td>");
-                        echo ("<td> <p>{$arrayItems[$i]->getQuantity()}</p> </td>");
-                        echo ("<td> <p>{$arrayItems[$i]->getPrice()}</p> </td>");
-                        echo ("<td> <p>{$total}</p> </td>");
-                    echo("</tr>");
+                    echo "<tr align='center'>
+                            <td> <p>{$arrayItems[$i]->getName()}</p> </td>
+                            <td> <p>{$arrayItems[$i]->getQuantity()}</p> </td>
+                            <td> <p>{$arrayItems[$i]->getPrice()}</p> </td>
+                            <td> <p>{$total}</p> </td>
+                        </tr>";
                 }
-                //</tr>
-            ?>
-            
-            <tr>
-                <td colspan="3">
-                    <p> <?php $items = count($arrayItems);
-                            if ($items == 1) { echo "Hay $items producto"; } 
-                            else { echo "Hay $items productos"; }
-                        ?> 
-                    </p>
-                </td>
-                <td>
-                    <p> <?php echo $totalItem?> </p>
-                </td>
-            </tr>
-        </table>
+                
+                    echo "<tr>
+                            <td colspan='3'>
+                                <p>";  $items = count($arrayItems);
+                                if ($items == 1) { echo "Hay $items producto"; } 
+                                else { echo "Hay $items productos"; }
+                            echo "</p>
+                            </td>
+                            
+                            <td>
+                                <p> $totalItem </p>
+                            </td>
+                        </tr>
 
+                    </table>";
+            }
+        ?>
     </body>
 </body>
 </html>
